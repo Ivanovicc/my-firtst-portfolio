22a12f0c6b51d7d240edde6326b08e93e1a8c5cf
Ivanovicc <ivanacosta456@gmail.com>
2021-03-04 19:42:23 -0300

    projects styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,54 +1,33 @@
 import React from 'react';
+import { IoChevronForward } from "react-icons/io5";
+import cart from '../cart.png';
+import movile from '../movile.png';
 
 const Projects = ({ id }) => {
   return (
     <section>
-      <div className="container" id={id}>
-        <div className="row">
+      <div className="container">
+        <div className="row" id={id}>
           <div className="col-sm-12">
             <div className="title-box text-center">
               <h2 className="title">Proyectos</h2>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <div className="work-box">
-              <a>
-                <div className="work-img">
-                  <img src="" alt="" />
-                </div>
-              </a>
-              <div className="work-content">
-                <div className="row">
-                  <div className="col-sm-8">
-                    <h3 className="w-title">E-Commerce</h3>
-                    <div className="w-more">
-                      <a className="btn-main"><span>Ver Detalles</span></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="col-lg-4 mr-auto ml-auto text-center">
+            <div className="circular--portrait-mini">
+              <img src={cart} />
             </div>
+            <h2 className="work-title">E-commerce</h2>
+            <p className="work-info">Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+            <a className="btn-main" href="#">detalles <IoChevronForward /></a>
           </div>
-          <div className="col-md-4">
-            <div className="work-box">
-              <a>
-                <div className="work-img">
-                  <img src="" alt="" />
-                </div>
-              </a>
-              <div className="work-content">
-                <div className="row">
-                  <div className="col-sm-8">
-                    <h3 className="w-title">MOBA Aplicacion Movil</h3>
-                    <div className="w-more">
-                      <a className="btn-main"><span>Ver Detalles</span></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <div className="col-lg-4 ml-auto mr-auto text-center">
+            <div className="circular--portrait-mini">
+              <img src={movile} />
             </div>
+            <h2 className="work-title">Billetera Virtual</h2>
+            <p className="work-info">Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+            <a className="btn-main" href="#">detalles <IoChevronForward /></a>
           </div>
         </div>
       </div>
